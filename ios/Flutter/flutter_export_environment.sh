8580f01b05c8529a30da3aea153361e6382fb305
Jack Maring <jackmaring1@gmail.com>
2020-09-04 20:16:54 -0500

    spotjob

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jackmaring/Documents/sdks/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jackmaring/Documents/Programming/GitHub/SpotJob"
-export "FLUTTER_TARGET=/Users/jackmaring/Documents/Programming/GitHub/SpotJob/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/jackmaring/Documents/Programming/GitHub/spotjob"
+export "FLUTTER_TARGET=/Users/jackmaring/Documents/Programming/GitHub/spotjob/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
